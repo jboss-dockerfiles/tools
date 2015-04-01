33c09c32f14e36af8787e1f1b0530b21853e26d0
Marek Goldmann <marek.goldmann@gmail.com>
2015-04-01 08:24:54 +0200

    Add license header + enable debug by default

--- test/docker_test_helpers.py
+++ test/docker_test_helpers.py
@@ -1,3 +1,27 @@
+"""
+The MIT License (MIT)
+
+Copyright (c) 2015 Red Hat
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 import logging
 import requests
 import select
@@ -9,12 +33,8 @@ from docker import Client
 d = Client()
 
 # FIXME
-logging.basicConfig()
-
+logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
-
-
-
 
 def _handle_request(container, port=80, expected_status_code=200, wait=30, timeout=0.5, expected_phrase=None):
     """
